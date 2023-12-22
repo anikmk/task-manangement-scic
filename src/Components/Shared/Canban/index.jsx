2017357c7921ce4fddaf893372979c95b6e85a74
anikmk <anikmollik122321@gmail.com>
2023-12-22 18:21:49 +0600

    dnd nor working

--- src/Components/Shared/Canban/index.jsx
+++ src/Components/Shared/Canban/index.jsx
@@ -1,10 +1,76 @@
-
+import React, { useState, useEffect } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import Card from "../../Card";
 
 const Kanban = () => {
+    const [drags, setDrags] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/drag')
+            .then(res => res.json())
+            .then(data => {
+                setDrags(data);
+            });
+    }, []);
+
+    const onDragEnd = (result) => {
+        console.log(result);
+        // Implement logic for onDragEnd if needed
+    };
+
     return (
-        <div>
-            kanban
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className="flex gap-5 justify-center items-start text-white p-7">
+                {drags.map((drag) => (
+                    <Droppable key={drag._id} droppableId={drag._id}>
+                        {(provided) => (
+                            <div
+                                className=""
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                <div className="w-[300px] p-8 rounded bg-green-600 text-center font-bold text-xl">
+                                    {drag.titles}
+                                </div>
+                                <div className="bg-black text-center p-6">
+                                    {drag.tasks &&
+                                    Array.isArray(drag.tasks) ? (
+                                        drag.tasks.map((task, index) => (
+                                            <Draggable
+                                                key={task._id}
+                                                draggableId={task._id}
+                                                index={index}
+                                            >
+                                                {(provided, snapshot) => (
+                                                    <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        style={{
+                                                            ...provided.draggableProps.style,
+                                                            opacity: snapshot.isDragging
+                                                                ? '0.5'
+                                                                : '1',
+                                                        }}
+                                                    >
+                                                        <Card>
+                                                            {task.title}
+                                                        </Card>
+                                                    </div>
+                                                )}
+                                            </Draggable>
+                                        ))
+                                    ) : (
+                                        <p>No tasks available</p>
+                                    )}
+                                </div>
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                ))}
+            </div>
+        </DragDropContext>
     );
 };
 
--- src/Page/DashBoard/SeePreviousTask.jsx
+++ src/Page/DashBoard/SeePreviousTask.jsx
@@ -4,11 +4,6 @@ import Kanban from "../../Components/Shared/Canban";
 const SeePreviousTask = () => {
     return (
         <div className="bg-slate-600">
-            <div className="bg-slate-950 flex justify-between items-center text-white p-5 rounded">
-                <h1 className="font-semibold">TO DO</h1>
-                <h1 className="font-semibold">ON GOING</h1>
-                <h1 className="font-semibold">COMPLETED</h1>
-            </div>
             <div>
                 <Kanban></Kanban>
             </div>
